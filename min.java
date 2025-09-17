dee06bfba979ac8748120a4adbfe774cbc3dd628
Dharshini847 <dharshi3483@gmail.com>
2025-09-17 13:56:21 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class minmax{
+    public static void main(String[]args){
+        Scanner minmax= new Scanner(System.in);
+        int num1=minmax.nextInt();
+        int num2=minmax.nextInt();
+        int num3=minmax.nextInt();
+        if(num1>num2) {
+            System.out.println("the num1 is maximum");
+            }
+            else{
+                System.out.println("the num1 is minimum");
+            }
+         if (num2>num1) {
+            System.out.println("the num2 is maximum");
+         }
+         else{
+            System.out.println("the num2 is minimum");
+         }
+    }
+}
